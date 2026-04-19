ddc5b348d6450f609e55acb22151f4744562e42b
areetampo <areebrawl@gmail.com>
2026-04-19 19:35:02 +0530

    compare page indefinite loading state bug fix

--- frontend/src/pages/AssessmentComparisonPage/AssessmentComparisonPage.jsx
+++ frontend/src/pages/AssessmentComparisonPage/AssessmentComparisonPage.jsx
@@ -52,7 +52,7 @@ export default function AssessmentComparisonPage() {
     );
   }
 
-  if (!isLoading) return <ComparisonSkeleton />;
+  if (isLoading) return <ComparisonSkeleton />;
 
   if (isError) {
     return (
