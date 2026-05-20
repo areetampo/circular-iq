a35ddd7a26aca99fad11580931f608f78c74b37d
areetampo <areebrawl@gmail.com>
2026-05-20 22:10:00 +0530

    Delete frontend/src/config/index.js barrel re-export (direct imports now used)

--- frontend/src/config/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @module config
- * @description Central export point for frontend configuration.
- * Re-exports FRONTEND_CONFIG for convenient importing.
- */
-
-export { FRONTEND_CONFIG } from './frontend.config';
